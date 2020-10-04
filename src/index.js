c87d9e1a3309311e117d2c695917f38f7fee1846
Andy Liu <aliu927@gmail.com>
2020-10-03 17:56:34 -0700

    learning react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+// Import the React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// Create a react component
+const App = () => {
+    return <div><h1>Hi There!</h1></div>;
+};
+
+//Take the react component and hosw it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        <App />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
